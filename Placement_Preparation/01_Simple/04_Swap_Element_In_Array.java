02073b9ecf234599fb4888e9983a3564dea7b55a
khuntkaran <94033442+khuntkaran@users.noreply.github.com>
2023-12-05 10:34:32 +0530

    Add files via upload

---
+++ Placement_Preparation/01_Simple/04_Swap_Element_In_Array.java
@@ -0,0 +1,14 @@
+public class Swap_Element_In_Array {
+    public static void main(String[] args) {
+        int[] arr={5,6,8,9,4,5,8,9};
+        int temp;
+        for(int i=0;i<arr.length;i=i+2){
+            temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+        for(int i :arr){
+            System.out.println(i);
+        }
+    }
+}
