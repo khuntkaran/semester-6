0408cca86d0c213d943ac5082698480303c321b2
khuntkaran <khuntkaran995@gmail.com>
2023-12-28 20:42:42 +0530

    .

---
+++ Advance DOTNET/TruckTransportManegment/TruckTransportManegment/Areas/Login/Controllers/ValidationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruckTransportManegment.Areas.Login.Controllers
+{
+    [Area("Login")]
+    public class ValidationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
