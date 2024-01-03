1a1b181e8e2f4a197b08ae90141c06a3e067f474
khuntkaran <khuntkaran995@gmail.com>
2024-01-03 11:27:06 +0530

    .

--- Advance DOTNET/TruckTransportManegment/TruckTransportManegment/Areas/Login/Models/UserModel.cs
+++ Advance DOTNET/TruckTransportManegment/TruckTransportManegment/Areas/Login/Models/UserModel.cs
@@ -5,16 +5,26 @@ namespace TruckTransportManegment.Areas.Login.Models
 {
     public class UserModel
     {
+        [Required]
         public int UserID { get; set; }
         [Required] 
         public string UserName { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
+        public string REPassword { get; set; }
+        [Required]
         public bool IsActive { get; set; }
+        [Required]
         public bool IsAdmin { get; set; }
+        [Required]
         public DateTime Created { get; set; }
-        public DateTime Modified { get; set; }
+            [Required]
+            public DateTime Modified { get; set; }
 
     }
 }
